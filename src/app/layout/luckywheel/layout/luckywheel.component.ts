abf35d05f795d1aa014160b63ef65ad4463df19c
Zin <quoctp@icheck.vn>
2020-09-16 18:39:19 +0700

    Fix step 4

--- src/app/layout/luckywheel/layout/luckywheel.component.ts
+++ src/app/layout/luckywheel/layout/luckywheel.component.ts
@@ -1,4 +1,5 @@
 import { Component } from "@angular/core";
+import { STEP_LANDING_PAGE } from 'src/app/public/constant/constant';
 
 @Component({
   selector: 'app-luckywheel',
@@ -8,58 +9,11 @@ import { Component } from "@angular/core";
 })
 export class LuckyWheelComponentPage {
   index = 0;
+  step = STEP_LANDING_PAGE;
   constructor() {
     console.log(this.step[this.index].button.length > 1);
   }
-  step = [
-    {
-      image: '../../../../assets/images/luckywheel/step1/group_2804.png',
-      image2x: '../../../../assets/images/luckywheel/step1/group_2804@2x.png',
-      image3x: '../../../../assets/images/luckywheel/step1/group_2804@3x.png',
-      title: 'Quét QR code, nhận cơ hội tốt',
-      note: 'Bạn ơi! Quét QR code trên sản phẩm để tham gia minigame có thưởng nhé!',
-      button: [
-        {
-          title: 'Tiếp tục',
-          acctive: true
-        }
-      ]
-    },
-    {
-      image: '../../../../assets/images/luckywheel/step2/group_2803.png',
-      image2x: '../../../../assets/images/luckywheel/step2/group_2803@2x.png',
-      image3x: '../../../../assets/images/luckywheel/step2/group_2803@3x.png',
-      title: 'Chơi game hay, quà liền tay',
-      note: 'Nhanh tay thử vận may với vòng quay may mắn nào bạn ơi!',
-      button: [
-        {
-          title: 'Tiếp tục',
-          acctive: true
-        },
-        {
-          title: 'Quay lại',
-          acctive: true
-        }
-      ]
-    },
-    {
-      image: '../../../../assets/images/luckywheel/step3/group_2802.png',
-      image2x: '../../../../assets/images/luckywheel/step3/group_2802@2x.png',
-      image3x: '../../../../assets/images/luckywheel/step3/group_2802@3x.png',
-      title: 'Wow! Trúng thưởng rồi!',
-      note: 'Thật tuyệt vời, bạn đã nhận được phần quà từ nhà tài trọ rồi đấy!',
-      button: [
-        {
-          title: 'Tôi đã hiểu',
-          acctive: true
-        },
-        {
-          title: 'Quay lại',
-          acctive: true
-        }
-      ]
-    }
-  ]
+  
   next(index) {
     if (index !== 2) {
       this.index = index + 1;
---
+++ src/app/public/constant/constant.ts
@@ -0,0 +1,49 @@
+export const STEP_LANDING_PAGE = [
+  {
+    image: '../../../../assets/images/luckywheel/step1/group_2804.png',
+    image2x: '../../../../assets/images/luckywheel/step1/group_2804@2x.png',
+    image3x: '../../../../assets/images/luckywheel/step1/group_2804@3x.png',
+    title: 'Quét QR code, nhận cơ hội tốt',
+    note: 'Bạn ơi! Quét QR code trên sản phẩm để tham gia minigame có thưởng nhé!',
+    button: [
+      {
+        title: 'Tiếp tục',
+        acctive: true
+      }
+    ]
+  },
+  {
+    image: '../../../../assets/images/luckywheel/step2/group_2803.png',
+    image2x: '../../../../assets/images/luckywheel/step2/group_2803@2x.png',
+    image3x: '../../../../assets/images/luckywheel/step2/group_2803@3x.png',
+    title: 'Chơi game hay, quà liền tay',
+    note: 'Nhanh tay thử vận may với vòng quay may mắn nào bạn ơi!',
+    button: [
+      {
+        title: 'Tiếp tục',
+        acctive: true
+      },
+      {
+        title: 'Quay lại',
+        acctive: true
+      }
+    ]
+  },
+  {
+    image: '../../../../assets/images/luckywheel/step3/group_2802.png',
+    image2x: '../../../../assets/images/luckywheel/step3/group_2802@2x.png',
+    image3x: '../../../../assets/images/luckywheel/step3/group_2802@3x.png',
+    title: 'Wow! Trúng thưởng rồi!',
+    note: 'Thật tuyệt vời, bạn đã nhận được phần quà từ nhà tài trọ rồi đấy!',
+    button: [
+      {
+        title: 'Tôi đã hiểu',
+        acctive: true
+      },
+      {
+        title: 'Quay lại',
+        acctive: true
+      }
+    ]
+  }
+]
